31ae5143b28e3d8dee860d0f1630ce6d461e17f2
takuro913 <atkgdsfhsk.jp@gmail.com>
2023-10-14 15:52:25 +0900

    一覧画面いじり

--- resources/views/clothes/index.blade.php
+++ resources/views/clothes/index.blade.php
@@ -14,7 +14,7 @@
                             <tr>
                                 <th class="py-4 px-6 bg-gray-lightest dark:bg-gray-darkest font-bold uppercase text-lg text-gray-dark dark:text-gray-200 border-b border-grey-light dark:border-grey-dark">服の名前</th>
                                 <th class="py-4 px-6 bg-gray-lightest dark:bg-gray-darkest font-bold uppercase text-lg text-gray-dark dark:text-gray-200 border-b border-grey-light dark:border-grey-dark">色</th>
-                                <th class="py-4 px-6 bg-gray-lightest dark:bg-gray-darkest font-bold uppercase text-lg text-gray-dark dark:text-gray-200 border-b border-grey-light dark:border-grey-dark">ジャンル</th>
+                                <th class="py-4 px-6 bg-gray-lightest dark:bg-gray-darkest font-bold uppercase text-lg text-gray-dark dark:text-gray-200 border-b border-grey-light dark:border-grey-dark"white-space: nowrap>ジャンル</th>
                                 <th class="py-4 px-6 bg-gray-lightest dark:bg-gray-darkest font-bold uppercase text-lg text-gray-dark dark:text-gray-200 border-b border-grey-light dark:border-grey-dark">画像</th>
                                 <th class="py-4 px-6 bg-gray-lightest dark:bg-gray-darkest font-bold uppercase text-lg text-gray-dark dark:text-gray-200 border-b border-grey-light dark:border-grey-dark">更新日時</th>
                             </tr>
@@ -22,12 +22,12 @@
                         <tbody>
                             @foreach ($clothes as $clothe)
                             <tr class="hover:bg-gray-lighter">
-                                <td class="py-4 px-6 border-b border-gray-light dark:border-gray-600">{{$clothe->name}}</td>
-                                <td class="py-4 px-6 border-b border-gray-light dark:border-gray-600">{{$clothe->color->name}}</td>
-                                <td class="py-4 px-6 border-b border-gray-light dark:border-gray-600">{{$clothe->genre->name}}</td>
+                                <td class="py-4 px-6 border-b border-gray-light dark:border-gray-600"white-space: nowrap>{{$clothe->name}}</td>
+                                <td class="py-4 px-6 border-b border-gray-light dark:border-gray-600"white-space: nowrap>{{$clothe->color->name}}</td>
+                                <td class="py-4 px-6 border-b border-gray-light dark:border-gray-600"white-space: nowrap>{{$clothe->genre->name}}</td>
                                 <td class="py-4 px-6 border-b border-gray-light dark:border-gray-600">
                                     <!--<img src="{{ asset('storage/' . $clothe->image) }}" alt="Clothe Image" class="w-16 h-16">-->
-                                     <img src="{{ Storage::url($clothe->image) }}" width="25%">
+                                     <img src="{{ Storage::url($clothe->image) }}" style="display: block; margin: auto;" width="25%">
                                 </td>
                                 <td class="py-4 px-6 border-b border-gray-light dark:border-gray-600">{{$clothe->updated_at}}</td>
                             </tr>
